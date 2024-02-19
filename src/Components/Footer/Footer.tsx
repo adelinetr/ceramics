be1e15583524b5652b8c74b426922b01b4c62838
Adelina <adelinatravina@gmail.com>
2024-02-19 19:26:40 +0200

    newsletter section

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,16 +3,16 @@ import Image from './Image.png'
 export default function Footer() {
   return (
     <footer className="h-fit pb-20 lg:pb-0 lg:pt-0 bg-[#EAEAEA] pt-12 border-t border-[#8F8F8F]">
-      <div className='flex flex-col lg:flex-row justify-center items-center lg:max-w-fit mx-auto lg:space-x-12'>
+      <div className='flex flex-col lg:flex-row justify-center items-center lg:max-w-fit mx-auto lg:space-x-20'>
 
         <div className="flex flex-col mx-5 lg:mx-0 lg:max-w-[250px] justify-center items-center lg:items-start">
-          <h3 className="text-3xl">Ceramic</h3>
-          <span className="font-archivo w-[90%] mt-4 lg:text-xs lg:w-[90%] text-[#535353]">
+          <h3 className="text-3xl font-archivo font-medium text-[#333333]">Ceramic</h3>
+          <span className="font-archivo w-[90%] mt-2 lg:text-xs lg:w-[90%] text-[#535353]">
             Ceramics Club strives to provide a space for creative individuals.
           </span>
         </div>
 
-        <div className='mx-0 flex flex-col lg:flex-row justify-center items-center space-y-8 mt-10 lg:mt-0 lg:gap-10'>
+        <div className='mx-0 flex flex-col lg:flex-row justify-center items-center space-y-8 mt-10 lg:mt-0 lg:gap-16'>
           <img className='w-full h-[32vh] object-cover lg:w-[280px] lg:h-[h-40vh]' src={Image} alt="" />
           <div className='flex flex-row justify-center items-start lg:pb-6 space-x-20 mx-auto py-6 lg:py-0'>
             <ul className='space-y-6 lg:space-y-3 text-lg lg:text-sm font-archivo'>
@@ -45,7 +45,7 @@ export default function Footer() {
           </div>
         </div>
 
-        <div className='mx-5 lg:hidden xl:flex mt-20 lg:mt-0 flex flex-col justify-center items-center lg:ml-24'>
+        <div className='mx-5 lg:hidden xl:flex lg:items-start mt-20 lg:mt-0 flex flex-col justify-center items-center lg:ml-24'>
           <h3 className='text-4xl font-archivo text-center mb-2 lg:text-2xl'>Subscribe to our newsletter</h3>
           <div className='flex flex-col w-full mt-6 space-y-3 lg:space-y-0 lg:flex-row lg:space-x-4'>
             <input
--- src/Components/Newsletter/Newsletter.tsx
+++ src/Components/Newsletter/Newsletter.tsx
@@ -1,24 +1,20 @@
-import Image from './01.png'
+import './Newsletter.css'
 
 export default function Newsletter() {
   return (
-    <section className="w-full h-screen flex flex-col justify-center lg:max-w-[1000px] xl:max-w-[1300px] mx-auto">
-      <div className="mx-5 lg:flex lg:flex-row items-center justify-center">
-        <div className="mt-2">
-          <h3 className="text-5xl text-center lg:text-left">Subscribe to our newsletter</h3>
-          <p className="mt-5 text-[#535353] lg:w-[50%]">
+    <section className="w-full flex flex-col justify-center mx-auto">
+      <div className="lg:flex bg-img h-screen lg:h-[60vh] lg:flex-row mx-auto items-center justify-center">
+        <div className="mt-2 mx-5 flex flex-col">
+          <h3 className="text-5xl text-center">Subscribe to our newsletter</h3>
+          <p className="mt-5 text-[#535353] lg:w-[50%] lg:text-center lg:mx-auto">
             Ceramics Club strives to provide a space for creative individuals
             who have a passion for pottery or an interest in learning more about
             the art form.
           </p>
-          <div className="flex flex-col w-full mt-6 space-y-3 lg:space-y-0 lg:flex-row lg:space-x-4">
-            <input className="border px-3 py-2 focus:outline-none lg:h-[5vh] rounded-lg lg:w-[60%]" type="text" placeholder="Enter your email" name="" id="" />
+          <div className="flex flex-col w-[90%] mx-auto mt-12 space-y-3 lg:space-y-0 lg:flex-row lg:space-x-4">
+            <input className="border px-3 py-2 focus:outline-none lg:h-[5vh] rounded-lg w-full mx-auto" type="text" placeholder="Enter your email" name="" id="" />
             <button className="border py-2 hover:shadow-xl transition-all duration-200 rounded-lg px-6 font-archivo uppercase bg-[#232323] text-white">Subscribe</button>
           </div>
-        </div>
-
-        <div>
-          <img className='rounded-lg md:rounded-none mt-6 h-[46vh] lg:h-[80vh] xl:h-[60vh] w-full object-cover' src={Image} alt="" />
         </div>
       </div>
     </section>
