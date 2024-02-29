4cac67d429843a1bb7aa1d77fc0d5fdc18b59256
Adelina <adelinatravina@gmail.com>
2024-02-29 14:55:45 +0200

    Created Image Buttons

--- src/Components/ServicesPage/ClassesPage/AboutClub/AboutClub.tsx
+++ src/Components/ServicesPage/ClassesPage/AboutClub/AboutClub.tsx
@@ -1,26 +1,44 @@
 import Image1 from "./Image1.png";
+import ImageButton from "./ImageButton";
+import Image2 from "./Image2.png";
+import Image3 from "./Image3.png";
 
 export default function AboutClub() {
   return (
-    <section className="w-full mx-auto mb-10">
+    <section className="w-full mx-auto mb-10 mt-16">
       <div className="w-[100%] relative">
         <img
-          className="h-screen md:h-[60vh] absolute w-[100%] object-cover z-[-10] brightness-50"
+          className="h-screen md:h-[80vh] absolute w-[100%] object-cover z-[-10] brightness-50"
           src={Image1}
           alt=""
         />
-        <div className="flex flex-col justify-center h-screen w-[90%] lg:w-[550px] md:w-[60%] mx-auto md:h-[60vh]">
-          <div className="bg-white px-10 py-8">
-            <h4 className="text-2xl font-archivo text-center uppercase mb-5">
+        <div className="flex flex-col justify-center items-center h-screen w-[90%] md:w-[100%] mx-auto md:h-[80vh]">
+          <div className="bg-white rounded-sm px-10 py-12 md:h-[300px] md:w-[650px]">
+            <h4 className="text-2xl text-[#515151] md:text-3xl tracking-widest font-archivo text-center uppercase mb-5">
               Pottery Club
             </h4>
-            <p className="md:w-[80%] font-archivo mx-auto text-[0.85rem] text-[#535353]">
+            <p className="md:w-[75%] font-archivo leading-6 mx-auto text-[0.85rem] md:text-base text-[#7a7a7a]">
               Founded with a love for pottery and a commitment to fostering
               creativity, we are a vibrant community of ceramic enthusiasts,
-              artists, and beginners alike.
+              artists, and beginners alike. Nestled in the heart of London, our
+              studio provides a welcoming space for individuals to explore the
+              art of pottery and unleash their artistic potential.
             </p>
           </div>
         </div>
+      </div>
+
+      <div className="flex flex-col lg:flex-row">
+        <ImageButton
+          image={Image2}
+          heading="Shop"
+          text="Buy new items for apartment."
+        />
+        <ImageButton
+          image={Image3}
+          heading="Studio Access"
+          text="Rent our studio for creative activities."
+        />
       </div>
     </section>
   );
---
+++ src/Components/ServicesPage/ClassesPage/AboutClub/ImageButton.tsx
@@ -0,0 +1,38 @@
+import Arrow from "./Arrow.svg";
+
+interface ImgButton {
+  image: string;
+  heading: string;
+  text: string;
+}
+
+export default function ImageButton(props: ImgButton) {
+  return (
+    <div>
+      <div className="lg:mx-1 cursor-pointer group relative lg:mt-1 flex my-1 mx-1 lg:my-0 justify-center items-center">
+        <div className=" overflow-hidden">
+          <img
+            className="h-[40vh] w-[100vw] brightness-50 group-hover:brightness-75 group-hover:scale-125  transition-all duration-200 lg:rounded-sm lg:w-[50vw] object-cover"
+            src={props.image}
+            alt=""
+          />
+        </div>
+        <div className="absolute flex flex-col">
+          <h3 className=" flex gap-4 uppercase text-white tracking-widest font-archivo text-3xl md:text-4xl font-light">
+            {props.heading}
+            <img
+              className="lg:invisible transition-all ease-in-out group-hover:visible"
+              src={Arrow}
+              alt=""
+            />
+          </h3>
+          <div>
+            <p className="lg:invisible group-hover:visible font-archivo font-extralight tracking-wider mt-2 text-base text-white">
+              {props.text}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
