6a8f25e24052108633d84ade176b265a8d4ab97b
Adelina <adelinatravina@gmail.com>
2024-03-06 11:28:09 +0200

    Added Event components

--- src/Components/ServicesPage/EventsPage/UpcomingEvents/EventComponent.tsx
+++ src/Components/ServicesPage/EventsPage/UpcomingEvents/EventComponent.tsx
@@ -1,23 +1,33 @@
-import Event1 from "./Event1.png";
+import Button from "../../../Header/Button";
 
-export default function EventComponent() {
+interface Event {
+  heading: string;
+  date: string;
+  image: string;
+  description: string;
+}
+
+export default function EventComponent(props: Event) {
   return (
     <>
-      <div className="mt-8 bg-[#EFECE5] pb-10 rounded-lg pt-1">
-        <img className="rounded-lg w-[97%] mx-auto" src={Event1} alt="" />
-        <div className="mt-2 mx-4">
-          <h4 className="font-archivo text-2xl text-[#3C3C3C]">
-            Introduction to Pottery
+      <div className="mt-8 relative flex flex-col lg:max-w-[1100px] lg:flex-row bg-[#F9F8F6] border lg:pb-0 pb-6 rounded-sm">
+        <img className="rounded-sm lg:w-[400px] mx-auto" src={props.image} alt="" />
+        <div className="mt-4 mx-4 lg:mx-10 lg:my-8">
+          <h4 className="font-archivo text-[1.8rem] leading-[2.7rem] lg:text-[2.2rem] text-[#3C3C3C]">
+            {props.heading}
           </h4>
           <span className="text-[#5F5F5F] uppercase text-[0.8rem] font-inter tracking-widest">
-            20th of march - 13:00-15:00
+            {props.date}
           </span>
-          <p className="font-archivo font-extralight mt- text-[#535353]">
-            Lorem ipsum dolor sit amet consectetur. Enim massa dictumst nullam
-            nam. In cum scelerisque egestas laoreet. Lorem ipsum dolor sit amet
-            consectetur. Enim massa dictumst nullam nam. In cum scelerisque
-            egestas laoreet.
+          <p className="font-archivo font-extralight xl:w-[65%] lg:text-[0.90rem] lg:w-[70%] leading-6 mt-5 text-[#535353]">
+            {props.description}
           </p>
+          <div>
+            <Button
+              name="Learn More"
+              className="bg-[#ffffff70] border lg:w-fit right-5 text-[#3C3C3C] lg:text-[0.85rem] lg:py-1.5 hover:bg-[#BBA081] hover:border-[#BBA081] hover:text-white duration-200 px-8 bottom-5 lg:absolute rounded-md border-[#A9A9A9] font-inter tracking-wider font-medium uppercase mt-8"
+            />
+          </div>
         </div>
       </div>
     </>
--- src/Components/ServicesPage/EventsPage/UpcomingEvents/UpcomingEvents.tsx
+++ src/Components/ServicesPage/EventsPage/UpcomingEvents/UpcomingEvents.tsx
@@ -1,14 +1,46 @@
 import EventComponent from "./EventComponent";
+import Event1 from "./Event1.png";
+import Event2 from "./Event2.png";
+import Event3 from "./Event3.png";
+import Event4 from "./Event4.png";
 
-export default function UpcomingEvents () {
-    return(
-        <section>
-            <div className="h-screen mx-4">
-                <h3 className="text-3xl mt-6 flex justify-center items-center w-[100%]">Upcoming Events
-                <span className=" border-[#3A3A3A] border-b ml-3 mt-2 w-[28vw]"></span>
-                </h3>
-                <EventComponent />
-            </div>
-        </section>
-    )
+export default function UpcomingEvents() {
+  return (
+    <section className="lg:max-w-[1000px] xl:max-w-[1100px] mx-auto">
+      <div className="h-fit my-20 mt-16 mx-4 lg:pt-6 lg:mx-auto">
+        <div className="flex justify-start space-x-3 items-center">
+          <h3 className="text-4xl lg:text-5xl mt-6 mb-6 flex justify-start items-center w-[60vw]">
+            Upcoming Events
+          </h3>
+          <span className=" border-[#3A3A3A] border-b ml-3 mt-2 w-[95vw]"></span>
+        </div>
+        <div className="mx-auto flex justify-center items-center flex-col">
+          <EventComponent
+            heading="Introduction to Pottery"
+            date="20th of march - 13:00-15:00"
+            description="Lorem ipsum dolor sit amet consectetur. Enim massa dictumst nullam nam. In cum scelerisque egestas laoreet. Lorem ipsum dolor sit amet consectetur. "
+            image={Event1}
+          />
+          <EventComponent
+            heading="Special Workshop: 'Mastering the Art of Ceramics'"
+            date="27th of march - 11:00-12:30"
+            description="Lorem ipsum dolor sit amet consectetur. Enim massa dictumst nullam nam. In cum scelerisque egestas laoreet. Lorem ipsum dolor sit amet consectetur. "
+            image={Event2}
+          />
+          <EventComponent
+            heading="Free Workshop: Art in Ceramics"
+            date="1st of april - 10:00-12:00"
+            description="Lorem ipsum dolor sit amet consectetur. Enim massa dictumst nullam nam. In cum scelerisque egestas laoreet. Lorem ipsum dolor sit amet consectetur. "
+            image={Event3}
+          />
+          <EventComponent
+            heading="Artistic Ceramics"
+            date="21st of april - 16:00-17:00"
+            description="Lorem ipsum dolor sit amet consectetur. Enim massa dictumst nullam nam. In cum scelerisque egestas laoreet. Lorem ipsum dolor sit amet consectetur. "
+            image={Event4}
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
